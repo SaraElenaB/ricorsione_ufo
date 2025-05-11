22106331023f819c707f28ad7ec3d650d24293e8
SaraElenaB <sara.bellotti03@gmail.com>
2025-05-11 14:12:08 +0200

    Lezione del 16/04/2025, controllare meglio il codice perchè non apre la pagina flet

--- model/modello.py
+++ model/modello.py
@@ -13,12 +13,15 @@ class Model:
         self._cammino_ottimo = []
         self._score_ottimo = 0
 
+    #------------------------------------------------------------------------------------------------------------------------------
     def get_years(self):
         return DAO.get_years()
 
+    # ------------------------------------------------------------------------------------------------------------------------------
     def get_shapes_year(self, year: int):
         return DAO.get_shapes_year(year)
 
+    # ------------------------------------------------------------------------------------------------------------------------------
     def create_graph(self, year: int, shape: str):
         self._grafo.clear()
         self._nodes = DAO.get_nodes(year, shape)
@@ -34,16 +37,13 @@ class Model:
                     weight = self._nodes[i].longitude - self._nodes[j].longitude
                     self._grafo.add_edge(self._nodes[j], self._nodes[i], weight= weight)
 
+    # ------------------------------------------------------------------------------------------------------------------------------
     def get_top_edges(self):
         sorted_edges = sorted(self._grafo.edges(data=True), key=lambda edge: edge[2].get('weight'), reverse=True)
         return sorted_edges[0:5]
 
-
     def get_nodes(self):
         return self._grafo.nodes()
-
-    # def get_edges(self):
-    #     return list(self._grafo.edges(data=True))
 
     def get_num_of_nodes(self):
         return self._grafo.number_of_nodes()
@@ -51,26 +51,30 @@ class Model:
     def get_num_of_edges(self):
         return self._grafo.number_of_edges()
 
+    # ------------------------------------------------------------------------------------------------------------------------------
     def cammino_ottimo(self):
         self._cammino_ottimo = []
         self._score_ottimo = 0
 
-        #TODO
-        for node in self._grafo.nodes():
-            parziale = [node]
+        for node in self._grafo.nodes(): #tutti i nodi del grafo
+            parziale = [node] #nodo di partenza
             rimanenti = self.calcola_rimanenti(parziale)
             self._ricorsione(parziale, rimanenti)
 
         return self._cammino_ottimo, self._score_ottimo
 
+    # ------------------------------------------------------------------------------------------------------------------------------
     def _ricorsione(self, parziale, nodi_rimanenti):
-        # caseo terminale:
+        #grafo: per def non ha ciclo
+
+        # caso terminale:
         if len(nodi_rimanenti) == 0:
             punteggio = self.calcola_punteggio(parziale)
             if punteggio > self._score_ottimo:
                 self._score_ottimo = punteggio
-                self._cammino_ottimo = copy.deepcopy(parziale)
-            print(parziale)
+                self.cammino_ottimo = copy.deepcopy(parziale)
+            #print(parziale)
+
         #caso ricorsivo
         else:
             #per ogni nodo rimanente
@@ -84,33 +88,27 @@ class Model:
                 #backtracking
                 parziale.pop()
 
-    def calcola_punteggio(self, parziale):
-        punteggio = 0
-        # termine fisso
-        punteggio += 100*len(parziale)
-        #termine variabile
-        for i in range(1, len(parziale)):
-            nodo = parziale[i]
-            nodo_precedente = parziale[i-1]
-            if nodo.datetime.month == nodo_precedente.datetime.month:
-                punteggio +=200
-        #return
-        return punteggio
-
+    # ------------------------------------------------------------------------------------------------------------------------------
     def calcola_rimanenti(self, parziale):
+
+        #nuovi_rimanenti = self._grafo.successors(parziale[-1]) #funzione che dato il grafo (l'ultimo nodo che abbiamo messo nel parziale) --> trova i successivi
+
         nuovi_rimanenti = []
         # prendiamo i nodi successivi
         for i in self._grafo.successors(parziale[-1]):
             # di questi nodi, dobbiamo verificare il vincolo sul mese
-            if (self.is_vincolo_ok(parziale, i) and
-                    self.is_vincolo_durata_ok(parziale, i)):
+            if (self.is_vincolo_ok(parziale, i) and self.is_vincolo_durata_ok(parziale, i) ):
                 nuovi_rimanenti.append(i)
         return nuovi_rimanenti
 
+    # ------------------------------------------------------------------------------------------------------------------------------
     def is_vincolo_durata_ok(self, parziale, nodo:Sighting):
-        return nodo.duration>parziale[-1].duration
 
-    def is_vincolo_ok(self, parziale, nodo:Sighting):
+        return nodo.duration > parziale[-1].duration #strettamente crescente
+
+    # ------------------------------------------------------------------------------------------------------------------------------
+    def is_vincolo_ok(self, parziale, nodo:Sighting): #trucco, se dici che nodo è un ogg Sightinh così poi ti aiuta dopo
+
         mese = nodo.datetime.month
         counter = 0
         for i in parziale:
@@ -121,4 +119,17 @@ class Model:
         else:
             return True
 
+    # ------------------------------------------------------------------------------------------------------------------------------
+    def calcola_punteggio(self, parziale):
+        punteggio = 0
 
+        #termine fisso
+        punteggio += 100*len(parziale)
+        #termine variabile
+        for i in range(1, len(parziale) ): #escludiamo il primo
+            nodo = parziale[i]
+            nodo_precedente = parziale[i-1]
+            if nodo.datetime.month == nodo_precedente.datetime.month:
+                punteggio += 200
+
+        return punteggio
